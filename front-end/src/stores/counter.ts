8c8ca2a8583c7017cb9e5930849375895a3ce219
Swan Htet Aung <swanhtetaung308@gmail.com>
2024-09-05 13:19:46 +0630

    sign and login hanler add

--- front-end/src/stores/counter.ts
+++ front-end/src/stores/counter.ts
@@ -1,12 +1,27 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed, reactive } from "vue";
+import { defineStore } from "pinia";
+import axios from "axios";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const useCounterStore = defineStore("main", () => {
+  let user = reactive({});
 
-  return { count, doubleCount, increment }
-})
+  const login = async (obj: Object) => {
+    try {
+      const res = await axios.post(`api/login`, obj);
+      Object.assign(user, res.data.user);
+      return res.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const signUp = async (obj: Object) => {
+    try {
+      const res = await axios.post(`api/sign-up`, obj);
+      return res.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+  return { user, login, signUp };
+});
